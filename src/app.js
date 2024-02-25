3512f52909387b341f8502590b921a9cfa3e3e4c
mrx7355608 <mrx7355608@gmail.com>
2024-02-25 14:21:35 +0500

    mounted auth router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import hpp from "hpp";
 import morgan from "morgan";
 import { catch404, globalErrorHandler } from "./utils/errorHandlers.js";
+import authRouter from "./routes/auth.js";
 
 const app = express();
 
@@ -17,6 +18,9 @@ app.use(
     }),
 );
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/auth", authRouter);
 
 // ERROR HANDLERS
 app.use(catch404);
