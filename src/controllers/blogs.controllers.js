50a7e972609f2e1f4f101ef1745a670633e17187
mrx7355608 <mrx7355608@gmail.com>
2024-02-25 19:07:53 +0500

    refactored blogs code in services

--- src/controllers/blogs.controllers.js
+++ src/controllers/blogs.controllers.js
@@ -1,22 +1,15 @@
-import BlogModel from "../models/blog.model.js";
 import catchAsyncError from "../utils/catchAsyncError.js";
-import ApiError from "../utils/ApiError.js";
-import blogValidator from "../validators/blog.validator.js";
-import slugify from "slugify";
+import {
+    addBlog,
+    editBlog,
+    listAllBlogs,
+    listOneBlogById,
+    removeBlog,
+} from "../services/blogs.services.js";
 
 const getAllBlogs = catchAsyncError(async (httpObject) => {
     const page = httpObject.query.page * 1 || 1;
-    const limit = 10;
-    const skip = (page - 1) * limit;
-
-    const blogs = await BlogModel.find(
-        { is_published: true },
-        "-__v -is_published",
-    )
-        .sort("-createdAt")
-        .limit(limit)
-        .skip(skip);
-
+    const blogs = await listAllBlogs(page);
     return {
         status: 200,
         data: blogs,
@@ -25,13 +18,7 @@ const getAllBlogs = catchAsyncError(async (httpObject) => {
 
 const getBlogById = catchAsyncError(async (httpObject) => {
     const blogID = httpObject.params.id;
-
-    // Check if blog exists
-    const blog = await BlogModel.findById(blogID);
-    if (blog === null) {
-        throw new ApiError("Blog not found", 404);
-    }
-
+    const blog = await listOneBlogById(blogID);
     return {
         status: 200,
         data: blog,
@@ -40,17 +27,7 @@ const getBlogById = catchAsyncError(async (httpObject) => {
 
 const createNewBlog = catchAsyncError(async (httpObject) => {
     const data = httpObject.body;
-
-    // Validate blog data
-    blogValidator(data);
-
-    // Create a new blog in database
-    const newBlog = await BlogModel.create({
-        ...data,
-        published_on: new Date(Date.now()).toDateString(),
-        slug: slugify(data.title),
-    });
-
+    const newBlog = await addBlog(data);
     return {
         status: 201,
         data: newBlog,
@@ -60,21 +37,7 @@ const createNewBlog = catchAsyncError(async (httpObject) => {
 const updateBlog = catchAsyncError(async (httpObject) => {
     const blogID = httpObject.params.id;
     const newBlogData = httpObject.body;
-
-    // Validate new blog data
-    blogValidator(newBlogData);
-
-    // Check if blog exists
-    const blog = await BlogModel.findById(blogID);
-    if (!blog) {
-        throw new ApiError("Blog not found", 404);
-    }
-
-    // Update blog
-    const updatedBlog = await BlogModel.findByIdAndUpdate(blogID, newBlogData, {
-        new: true,
-    });
-
+    const updatedBlog = await editBlog(blogID, newBlogData);
     return {
         status: 200,
         data: updatedBlog,
@@ -83,15 +46,7 @@ const updateBlog = catchAsyncError(async (httpObject) => {
 
 const deleteBlog = catchAsyncError(async (httpObject) => {
     const blogID = httpObject.params.id;
-
-    // Check if blog exists
-    const blog = await BlogModel.findById(blogID);
-    if (!blog) {
-        throw new ApiError("Blog not found", 404);
-    }
-
-    // Delete blog
-    await BlogModel.findByIdAndDelete(blogID);
+    await removeBlog(blogID);
     return {
         status: 204,
         data: null,
---
+++ src/services/blogs.services.js
@@ -0,0 +1,75 @@
+import BlogModel from "../models/blog.model.js";
+import blogValidator from "../validators/blog.validator.js";
+import slugify from "slugify";
+import ApiError from "../utils/ApiError.js";
+
+const listAllBlogs = async (page) => {
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const blogs = await BlogModel.find(
+        { is_published: true },
+        "-__v -is_published",
+    )
+        .sort("-createdAt")
+        .limit(limit)
+        .skip(skip);
+
+    return blogs;
+};
+
+const listOneBlogById = async (blogID) => {
+    // Check if blog exists
+    const blog = await BlogModel.findById(blogID);
+    if (blog === null) {
+        throw new ApiError("Blog not found", 404);
+    }
+
+    return blog;
+};
+
+const addBlog = async (data) => {
+    // Validate blog data
+    blogValidator(data);
+
+    // Create a new blog in database
+    const newBlog = await BlogModel.create({
+        ...data,
+        published_on: new Date(Date.now()).toDateString(),
+        slug: slugify(data.title),
+    });
+
+    return newBlog;
+};
+
+const editBlog = async (blogID, newBlogData) => {
+    // Validate new blog data
+    blogValidator(newBlogData);
+
+    // Check if blog exists
+    const blog = await BlogModel.findById(blogID);
+    if (!blog) {
+        throw new ApiError("Blog not found", 404);
+    }
+
+    // Update blog
+    const updatedBlog = await BlogModel.findByIdAndUpdate(blogID, newBlogData, {
+        new: true,
+    });
+
+    return updatedBlog;
+};
+
+const removeBlog = async (blogID) => {
+    // Check if blog exists
+    const blog = await BlogModel.findById(blogID);
+    if (!blog) {
+        throw new ApiError("Blog not found", 404);
+    }
+
+    // Delete blog
+    await BlogModel.findByIdAndDelete(blogID);
+    return null;
+};
+
+export { listAllBlogs, listOneBlogById, addBlog, editBlog, removeBlog };
