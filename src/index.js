d911cdec1314400591251c9b2bd0fb79688ad2b7
mrx7355608 <mrx7355608@gmail.com>
2024-03-10 19:51:30 +0500

    fixed an import statement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import app from "./app.js";
-import connectToDatabase from "./utils/db.js";
+import { connectToDatabase } from "./utils/db.js";
 
 const port = process.env.PORT || 8000;
 
