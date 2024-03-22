c6b53ae2797bfd2f9ed0ff7766b28931b8cb4c44
mrx7355608 <mrx7355608@gmail.com>
2024-03-22 05:32:26 +0500

    serving react app with express (4)

---
+++ dist/assets/DraftBlogs-C6723Fla.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CLWBLKRV.js";import{I as t,A as i}from"./AdminBlogCard-FyBlf0qn.js";import"./useAuthFetch-CPLIqvMY.js";import"./blog.services-B9F6rO5L.js";function e(){return r.jsx("div",{className:"flex items-start w-full flex-col gap-3 p-6",children:r.jsx(t,{endpoint:"/api/blogs/un-published",Card:i})})}export{e as default};
---
+++ dist/assets/PublishedBlogs-DGyODe5L.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-CLWBLKRV.js";import{I as s,A as l}from"./AdminBlogCard-FyBlf0qn.js";import"./useAuthFetch-CPLIqvMY.js";import"./blog.services-B9F6rO5L.js";function a(){return i.jsx("div",{className:"flex items-start w-full flex-col gap-3 p-6",children:i.jsx(s,{endpoint:"/api/blogs/published",Card:l})})}export{a as default};
